83a0b67d222289ac519017455085f563b07e8294
Nick Ahn <nickahn01@gmail.com>
2024-04-10 00:58:52 -0700

    Add optional which-key plugin

--- lua/nickel/lazy.lua
+++ lua/nickel/lazy.lua
@@ -30,6 +30,21 @@ local plugins = {
     },
     -- indent lines
     { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+    -- which key
+    --{
+    --  "folke/which-key.nvim",
+    --  event = "VeryLazy",
+    --  init = function()
+    --    vim.o.timeout = true
+    --    vim.o.timeoutlen = 300
+    --  end,
+    --  opts = {
+    --    -- your configuration comes here
+    --    -- or leave it empty to use the default settings
+    --    -- refer to the configuration section below
+    --  }
+    --}
+
 }
 local opts = {}
 
